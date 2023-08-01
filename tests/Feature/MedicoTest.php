92ac49fe2978938cdf38970c8f35baf8661145a7
Nivs <nivsoficial@gmail.com>
2023-08-01 20:51:11 -0300

    feat: atts about medico tests

--- tests/Feature/MedicoTest.php
+++ tests/Feature/MedicoTest.php
@@ -2,18 +2,34 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Database\Seeders\{PacienteSeeder, MedicoPacienteSeeder, MedicoSeeder, CidadeSeeder};
 
 class MedicoTest extends TestCase
 {
     /**
      * A basic feature test example.
      */
-    public function test_should_i_can_create_medico(): void
+    use RefreshDatabase;
+
+    public function test_should_can_i_use_medicos_list_endpoint(): void
     {
-        $response = $this->get('/cidades');
+        $response = $this->get('/medicos');
         $response->assertStatus(200);
+    }
+
+    public function test_should_return_medicos_list_endpoint(): void
+    {
+        $this->seed(MedicoSeeder::class); 
+        $response = $this->get('/medicos');
+
+        $response->assertStatus(200);
+                
+        // $this->assertCount(10, json_decode($response->getContent()));
+        //or...
+        $response->assertJsonCount(10);
     }
 }
