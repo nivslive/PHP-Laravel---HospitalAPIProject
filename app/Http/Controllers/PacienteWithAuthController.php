e28dd606d3d0b789205b054198e1ab672e9f31e2
Nivs <nivsoficial@gmail.com>
2023-08-02 12:32:10 -0300

    feat/fix: att about update paciente

--- app/Http/Controllers/PacienteWithAuthController.php
+++ app/Http/Controllers/PacienteWithAuthController.php
@@ -27,19 +27,18 @@ class PacienteWithAuthController extends Controller
         
     public function update(UpdatePacienteWithAuthRequest $request, $paciente_id) // : ResponseFactory | JsonResponse 
     {
-        dump($request);
-        // $paciente = Paciente::where('id', $paciente_id);
-        // if(!$paciente) {
-        //     return response()->json(['message' => 'Não há esse paciente cadastrado na base de dados!'], 404); 
-        // }
 
-        // $result = $paciente->update($request->except('_token'));
+        $paciente = Paciente::where('id', $paciente_id);
+        if(!$paciente) {
+            return response()->json(['message' => 'Não há esse paciente cadastrado na base de dados!'], 404); 
+        }
 
-        // if (!$result) {
-        //     return response()->json(['message' => 'Erro no preenchimento dos dados ao criar um médico'], 500);
-        // }
+        $result = $paciente->update($request->except('_token'));
+        if (!$result) {
+            return response()->json(['message' => 'Erro no preenchimento dos dados ao criar um médico'], 500);
+        }
 
-        // return response()->json(['message' => 'Você criou um paciente com sucesso!'], 200);
+        return response()->json(['message' => 'Você criou um paciente com sucesso!'], 200);
     }
 
 }
